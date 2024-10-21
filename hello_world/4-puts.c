86aa5d0fc06c4b88e0111404f37645389cbe448d
william <joshthefunnyalr@gmail.com>
2024-10-21 14:25:23 -0400

    added comments/description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: this is a test
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+
+	puts("\"Programming is like building a mutilingual puzzle");
 
 			return (0);
 }
