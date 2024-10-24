d20f52a6a444f498b5711381acbd9a6fe3cff276
william <joshthefunnyalr@gmail.com>
2024-10-24 09:40:17 -0400

    adding new line

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int number;
 
-		for (number = 0; number <= 9; number++)
+		for (number = 0; number < 10; number++)
 	{
-		printf("%d", number);
+		printf("%d\n", number);
 	}
 
 	return (0);
