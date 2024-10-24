2e95092efc67ccfe8bfc70086ef0ea07f7a519d5
william <joshthefunnyalr@gmail.com>
2024-10-24 09:34:51 -0400

    printing 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - printing 0 to 9
+ *
+ * Return: Always 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int number;
+
+		for (number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+
+	return (0);
+}
