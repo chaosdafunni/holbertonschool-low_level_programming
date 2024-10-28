3ed2d1daf01edb23857e1cb5c1d4af7d641e77f8
william <joshthefunnyalr@gmail.com>
2024-10-28 14:04:41 -0400

    printing a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+/**
+ * print_diagonal - printing diagonal
+ *
+ * @n: int used to check
+ *
+ */
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (b == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
