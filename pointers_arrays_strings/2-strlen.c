ee21aab210b3e13a036719352be3332a7242b01f
william <joshthefunnyalr@gmail.com>
2024-10-30 14:10:30 -0400

    printing the return of a string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/**
+ * _strlen - printing lenght
+ *
+ * @s: variable used to check
+ *
+ * Return: ll
+ */
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s)
+
+{
+	size_t ll = 0;
+
+	while (*s++)
+
+	ll++;
+
+	return (ll);
+
+}
