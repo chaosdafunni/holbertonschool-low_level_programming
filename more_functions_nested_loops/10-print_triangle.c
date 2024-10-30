cb8f94afd4277249d1422f3b9eb16765318dd53c
william <joshthefunnyalr@gmail.com>
2024-10-30 10:19:07 -0400

    adding a func that makes a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+/**
+ * print_triangle - printing a triangle with #
+ *
+ *@size: int used to check
+ *
+ * Return: void
+ *
+ */
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b > 0; b--)
+				_putchar(' ');
+
+			for (b = 0; b < a; b++)
+				_putchar('#');
+
+			if (a == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
+
