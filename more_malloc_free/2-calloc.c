2c02baadb527ba0bd97d6678d07f2c43ee3580b0
william <joshthefunnyalr@gmail.com>
2024-11-17 22:32:36 -0400

    fixing betty

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -26,7 +26,7 @@ char *_memset(char *s, char b, unsigned int n)
  * @nmemb: number of elements in the array
  * @size: size of each element
  *
- * Return: pointer 
+ * Return: pointer
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
