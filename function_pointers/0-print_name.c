26ef0dc68dded529dc42aa59df187a3fa92590e4
william <joshthefunnyalr@gmail.com>
2024-11-19 10:40:15 -0400

    adding everything needed for task 0 and after

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+/**
+ * print_name - function to print a name
+ * @name: name being printed
+ * @f: pointer to the func
+ */
+#include "function_pointers.h"
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (!name && !f)
+	{
+		return;
+	}
+	f(name);
+}
