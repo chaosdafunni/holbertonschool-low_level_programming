2eb24355921edf3b0e772f9ea998d4824796bb44
william <joshthefunnyalr@gmail.com>
2024-11-21 10:01:17 -0400

    fixing betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,21 @@
+/**
+ * array_iterator - executes a func given as a parameter
+ *
+ * @array: array used
+ * @size: size of the array used
+ * @action: pointer used
+ *
+ * Return: void
+ */
 #include "function_pointers.h"
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 
-{	
+{
 	unsigned int i;
 
 	if (array && action)
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
