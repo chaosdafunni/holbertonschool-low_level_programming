acc0a1c1cd9fc06f3e780e759525aab0cbca3d58
william <joshthefunnyalr@gmail.com>
2024-11-24 21:14:17 -0400

    adding a func that searches for ints

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+/**
+ *
+ * int_index - searches for int
+ *
+ * @array:array used to search
+ * @size: size of array
+ * @cmp:pointer
+ *
+ * Return: -1
+ */
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}
